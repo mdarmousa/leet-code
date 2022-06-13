c81cfb05cfaeef62eca45b85c5561e5ec00e57ef
Mohammad Darmousa <mdarmousa1@gmail.com>
2022-06-13 09:12:41 +0400

    Time: 6 ms (40.34%), Space: 44.5 MB (24.79%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,21 @@
+/*
+
+Go from bottom to top.
+
+We start form the row above the bottom row [size()-2].
+
+Each number add the smaller number of two numbers that below it.
+
+And finally we get to the top we the smallest sum.
+
+*/
+
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for(int i = triangle.size() - 2; i >= 0; i--)
+            for(int j = 0; j <= i; j++)
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+        return triangle.get(0).get(0);
+    }
+}
