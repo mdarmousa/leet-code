f50a3ccdcdaffe934f380be3e0285e5e32661a6f
Mohammad Darmousa <mdarmousa1@gmail.com>
2022-07-11 11:48:12 +0400

    Time: 10 ms (72.22%), Space: 48.4 MB (62.59%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -3,15 +3,15 @@ class Solution {
 
         if (numRows == 1) return s;
 
-        List<StringBuilder> rows = new ArrayList<>();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++)
-            rows.add(new StringBuilder());
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            rows[i] =new StringBuilder();
 
         int curRow = 0;
         boolean goingDown = false;
 
         for (char c : s.toCharArray()) {
-            rows.get(curRow).append(c);
+            rows[curRow].append(c);
             if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
             curRow += goingDown ? 1 : -1;
         }
