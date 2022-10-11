e3b2c4c50ceb267f07b00536bc21eb4e661d67ba
Mohammad Darmousa <mdarmousa1@gmail.com>
2022-10-11 20:21:31 +0400

    Time: 3 ms (77.00%), Space: 94.4 MB (16.94%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -5,10 +5,10 @@ class Solution {
         
         for(int i=0; i < nums.length; i++){
             if(nums[i] <  min1){
-                min1 = nums[i]; continue;
+                min1 = nums[i];
             }
             if(nums[i] < min2 &&  nums[i] >  min1){
-                min2 = nums[i];  continue;
+                min2 = nums[i];
             }
             if(nums[i] > min2 &&  nums[i] > min1){
                 return true;
